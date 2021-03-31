e9e87294f2bcae1420a436afe3688bf92b775526
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-03-31 16:38:25 +0200

    Add js functions in Splash.js component

--- src/js/components/Splash.js
+++ src/js/components/Splash.js
@@ -1,8 +1,38 @@
+import { select } from '../settings.js';
+
 class Splash {
   constructor(element) {
     const thisSplash = this;
 
     thisSplash.getElement(element);
+    thisSplash.handleWindowInnerWidthCheck();
+    thisSplash.setSliderPostsInPage(window.innerWidth);
+    thisSplash.setSliderPostsPageCount();
+
+    thisSplash.sliderCurrentPage = 0;
+
+    console.log(thisSplash.sliderPostsPageCount);
+  }
+
+  handleWindowInnerWidthCheck() {
+    window.addEventListener('resize', () => {
+      if(window.innerWidth > 0) {
+        this.setSliderPostsInPage(window.innerWidth);
+        this.setSliderPostsPageCount();
+      }
+    });
+  }
+
+  setSliderPostsInPage(innerWidth) {
+    const thisSplash = this;
+
+    if(innerWidth >= 950) thisSplash.sliderPostsInPage = 3;
+    else thisSplash.sliderPostsInPage = 1;
+  }
+
+  setSliderPostsPageCount() {
+    const thisSplash = this;
+    thisSplash.sliderPostsPageCount = Math.ceil(thisSplash.dom.splashSliderElements.length/thisSplash.sliderPostsInPage);
   }
 
   getElement(element) {
@@ -10,6 +40,9 @@ class Splash {
 
     thisSplash.dom = {};
     thisSplash.dom.wrapper = element;
+    thisSplash.dom.splashSliderElements = thisSplash.dom.wrapper.querySelectorAll(select.splashComponent.splashSliderElements);
+    thisSplash.dom.splashSliderElementsActive = thisSplash.dom.wrapper.querySelectorAll(select.splashComponent.splashSliderElementsActive);
+    console.log(thisSplash.dom.splashSliderElementsActive);
   }
 }
 
